f5800074858cd7d7f3bc1a8fd2de33b387f34da5
chapshaun <chapshaun@yahoo.com>
2015-10-12 08:48:12 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var express 				= require('express')
+	, exphbs					= require('express-handlebars')
+	, path						= require('path')
+  , port     				= 3000
+	, indexRoutes			= require('./routes/index')
+	, userRoutes			= require('./routes/user')
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes)
+app.use('/user', userRoutes)
+
+app.listen(port)
+
+console.log('Server running at http:127.0.0.1:' + port + '/')
