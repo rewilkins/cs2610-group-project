54f96c3b4d6cc6b00466c2c9a8bb188a60483d38
Swede Struve <swede@swedestruve.com>
2015-10-13 22:01:23 -0600

    Adding search route to app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express 				= require('express')
-	, exphbs					= require('express-handlebars')
-	, path						= require('path')
-  , port     				= 3000
-	// , indexRoutes			= require('./routes/index')
+	, exphbs			= require('express-handlebars')
+	, path				= require('path')
+	, port     			= 3000
+	// , indexRoutes		= require('./routes/index')
 	, userRoutes			= require('./routes/userRoutes')
 	, loginRoute			= require('./routes/loginRoute')
+	, searchRoute			= require('./routes/searchRoute')
 
 
 var app = express();
@@ -17,6 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRoutes)
 app.use('/user', userRoutes)
 app.use('/login', loginRoute)
+app.use('/search', searchRoute)
+
 
 app.listen(port)
 
