e8c5c4780bc619aa1d0bf0e3f62ae973af86ba82
Robert Wilkins <r.e.wilkins@gmail.com>
2015-11-11 15:14:45 -0700

    code maintenance for app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express 				= require('express')
-	, exphbs					= require('express-handlebars')
-	, request 				= require('request')
-	, bodyParser			= require('body-parser')
+var express 			= require('express')
+	, exphbs			= require('express-handlebars')
+	, request 			= require('request')
+	, bodyParser		= require('body-parser')
 	, querystring 		= require('querystring')
 	, session     		= require('express-session')
-	, path						= require('path')
+	, path				= require('path')
 	, cfg         		= require('./config')
-	, indexRoute			= require('./routes/indexRoute')
-	, userRoute				= require('./routes/userRoute')
-	, searchRoute			= require('./routes/searchRoute')
+	, indexRoute		= require('./routes/indexRoute')
+	, userRoute			= require('./routes/userRoute')
+	, searchRoute		= require('./routes/searchRoute')
 	, dashboardRoute	= require('./routes/dashboardRoute')
 	, profileRoute		= require('./routes/profileRoute')
-	, port     				= 3000
+	, port     			= 3000
 
 var app = express();
 
@@ -42,25 +42,25 @@ app.get('/login', function(req, res) {
 })
 
 app.get('/auth/finalize', function(req, res) {
-var post_data = {
-	client_id: cfg.client_id,
-	client_secret: cfg.client_secret,
-	redirect_uri: cfg.redirect_uri,
-	grant_type: 'authorization_code',
-	code: req.query.code
-}
+  var post_data = {
+	  client_id: cfg.client_id,
+	  client_secret: cfg.client_secret,
+	  redirect_uri: cfg.redirect_uri,
+	  grant_type: 'authorization_code',
+	  code: req.query.code
+  }
 
-	var options = {
-		url: 'https://api.instagram.com/oauth/access_token',
-		form: post_data
-	}
+  var options = {
+	  url: 'https://api.instagram.com/oauth/access_token',
+	  form: post_data
+  }
 
-	//for dashboard
-	request.post(options, function(error, response, body) {
-		var data = JSON.parse(body)
-		req.session.access_token = data.access_token
-		res.redirect('/dashboard')
-	})
+  //for dashboard
+  request.post(options, function(error, response, body) {
+	  var data = JSON.parse(body)
+	  req.session.access_token = data.access_token
+	  res.redirect('/dashboard')
+  })
 })
 
 	//for dashboard
