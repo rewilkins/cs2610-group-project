feabe08bd7f0c8ca8a5fa0a889243ffbd7ee200a
Shaun Chap <chapshaun@yahoo.com>
2015-11-11 08:52:18 -0700

    fixed uri

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var config = {}
 // Shaun's tokens
 config.client_id = '1c1c18cbde554eaba91bee06ff3b5921'
 config.client_secret = 'bb6731b3f7784bbfb5027418e17b2a1e'
-config.redirect_uri = 'http://127.0.0.1:3000/auth/finalize'
+config.redirect_uri = 'http://localhost:3000/auth/finalize'
 
 module.exports = config
