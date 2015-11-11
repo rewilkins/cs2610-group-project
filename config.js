52c121dcebf0dbb7dda99e2ef820e31851ab35e2
Robert Wilkins <r.e.wilkins@gmail.com>
2015-11-11 15:57:46 -0700

    Rob's config tokens

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 var config = {}
 
+// Rob's tokens
+//config.client_id = '76ab901902674b5ebfd4386dc6c065d4'
+//config.client_secret = 'f835c7a897704c6890ac15e1bcb93ce0'
+
 // Shaun's tokens
 config.client_id = '1c1c18cbde554eaba91bee06ff3b5921'
 config.client_secret = 'bb6731b3f7784bbfb5027418e17b2a1e'
