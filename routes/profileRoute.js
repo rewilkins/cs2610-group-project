582a3197601a46c36916638ddb66784da71cce2c
Robert Wilkins <r.e.wilkins@gmail.com>
2015-11-11 16:03:38 -0700

    Profile tab requires login to view

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profile', {
-    title: 'Profile'
-
+  router.get('/', function(req, res) {
+    //console.log(req.session.access_token)
+    if(req.session.access_token == null){
+        res.render('index', {
+        title: 'Home'
+      })
+    }
+    else{
+        res.render('profile', {
+        title: 'Profile'
+      })
+    }
   })
-})
 
 module.exports = router
