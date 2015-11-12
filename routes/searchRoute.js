b7ddb4c3b9e9d3a5638c3a5d62447fbd5dd8c7a0
Swede Struve <swede.struve@aggiemail.usu.edu>
2015-11-11 19:55:00 -0700

    Search route doesn't crash anymore

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -4,34 +4,32 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 
-  console.log('search')
-  console.log(req.session.access_token)
+    console.log('search')
+    console.log(req.session.access_token)
 
-  if (req.session.access_token == null) {
-    res.render('index', {
-      title: 'Home'
-    })
-  } else {
-
-    var options = {
-      url: 'https://api.instagram.com/v1/users/self/feed?access_token=' + req.session.access_token
-
-    }
-
-    request.get(options, function(error, response, body) {
-
-      var results = JSON.parse(body)
-
-      res.render('search', {
-        body: results.data
-
+    if (req.session.access_token == null) {
+      res.render('index', {
+        title: 'Home'
       })
+    } else {
+
+      var options = {
+        url: 'https://api.instagram.com/v1/users/self/feed?access_token=' + req.session.access_token
+
+      }
+
+      request.get(options, function(error, response, body) {
+
+        var results = JSON.parse(body)
+
+        res.render('search', {
+          title: 'Search',
+          body: results.data
+
+        })
+      })
+    }
     })
 
-    res.render('search', {
-      title: 'Search'
-    })
-  }
-})
 
 module.exports = router
