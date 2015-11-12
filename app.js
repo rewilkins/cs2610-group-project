dedc25fd5e814e2bbf506cf3b1fb2452a49017ed
Robert Wilkins <r.e.wilkins@gmail.com>
2015-11-12 12:06:38 -0700

    migrated dashboard components from app.js to dashboardRoute.js

--- app.js
+++ app.js
@@ -69,38 +69,7 @@ app.get('/auth/finalize', function(req, res) {
 })
 
 
-app.get('/dashboard', function (req, res, next){
-  
-  var options = {
-    url: 'https://api.instagram.com/v1/users/self/feed/?access_token=' + req.session.access_token
-  }
 
-  request.get(options, function(error, response, body) {
-	
-	if (error) {
-		console.log("error if 1")
-		return next(error)
-	  }
-	try {
-	  var feed = JSON.parse(body)
-	}
-    catch(err){
-	  console.log("error if 2")
-	  // return error if what we get back is HTML code
-	  return next(err) // displays the error on the page
-	  // return res.reditect('/') // just redirects to homepage
-	}
-
-	if (feed.meta.code > 200) {
-	  console.log("error code above 200")
-		return next(feed.meta.error_message)
-	  }
-	
-    res.render('dashboard', {
-      feed: feed.data
-    })
-  })
-})
 
 
 app.use(express.static(path.join(__dirname, 'public')));
