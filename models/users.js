46470d575c4d337cc777e525937a30956f3844e7
Swede Struve <swede.struve@aggiemail.usu.edu>
2015-12-07 09:01:55 -0700

    Added users.js file

---
+++ models/users.js
@@ -0,0 +1,40 @@
+var ObjectId = require('mongodb').ObjectId
+var assert = require('assert')
+var db = require('../db')
+
+exports.insert = function(user, callback) {
+  // Get the users collection
+  var collection = db.get().collection('users')
+  // Insert a user
+  collection.insert(user, function(err, result) {
+    assert.equal(err, null)
+    assert.equal(1, result.result.n)
+    assert.equal(1, result.ops.length)
+    console.log('Inserted 1 document into the users collection')
+    callback(result)
+  })
+}
+
+exports.find = function(id, callback) {
+  // Get the users collection
+  var collection = db.get().collection('users')
+  // Find a user
+  collection.findOne({'_id': ObjectId(id)}, function(err, document) {
+    assert.equal(err, null)
+    console.log('Found 1 user document')
+    callback(document)
+  })
+}
+
+exports.update = function(user, callback) {
+  // Get the users collection
+  var collection = db.get().collection('users')
+  user._id = ObjectId(user._id)
+  // Update the user
+  collection.update({'_id': user._id}, user, function(err, result) {
+    assert.equal(err, null)
+    assert.equal(1, result.result.n)
+    console.log('Updated 1 document in the users collection')
+    callback()
+  })
+}
