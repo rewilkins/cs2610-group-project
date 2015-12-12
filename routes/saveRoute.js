8d9c087e5617093ce10287560a0eb87cef9f7fb2
Swede Struve <swede.struve@aggiemail.usu.edu>
2015-12-11 21:12:35 -0700

    Working on new save route page

--- routes/saveRoute.js
+++ routes/saveRoute.js
@@ -5,6 +5,21 @@ var Users = require('../models/users');
 var router = express.Router();
 var SEARCH_QUERY = ''
 
+router.get('/', function(req, res) {
+  id = req.session.userId
+
+  Users.find(id, function(document) {
+    if (!document) {
+      res.redirect('/search')
+    } else {
+      document.tags = SEARCH_QUERY
+
+        res.render('save', {
+          tags: document.tags
+        })
+    }
+  })
+})
 
 router.post('/', function(req, res) {
   SEARCH_QUERY = req.body.query
